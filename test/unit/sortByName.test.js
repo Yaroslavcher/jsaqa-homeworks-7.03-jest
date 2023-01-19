e9eddf05b98962404daf5ca1977290b0581cfda8
unknown <yaroslavcher@gmail.com>
2023-01-19 22:30:12 +0300

    coverage data not found

---
+++ test/unit/sortByName.test.js
@@ -0,0 +1,24 @@
+const sorting = require("../../app");
+
+describe("Books names test suit", () => {
+  it("Books names should be sorted in ascending order", () => {
+    expect(
+      sorting.sortByName([
+        "Гарри Поттер",
+        "Властелин Колец",
+        "Волшебник изумрудного города",
+      ])
+    ).toEqual([
+      "Властелин Колец",
+      "Волшебник изумрудного города",
+      "Гарри Поттер",
+    ]);
+  });
+
+  it("Equal books names should not sorted", () => {
+    expect(sorting.sortByName(["Гарри Поттер", "Гарри Поттер"])).toEqual([
+      "Гарри Поттер",
+      "Гарри Поттер",
+    ]);
+  });
+});
